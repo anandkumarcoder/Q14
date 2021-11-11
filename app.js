c656682162e7f342b6dda5fd1d6b298d2ad182ec
anandkumarcoder <akanand104@gmail.com>
2021-11-11 21:36:31 +0530

    inital commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const button = document.querySelector(".btn");
+
+let objectOne = {
+    name: "Ram",
+    age: 25,
+    yoga: "Treta"
+}
+
+let objectTwo = {
+    name: "Krishna",
+    age: 31,
+    yoga: "Dwapar"
+}
+
+function comparison (){
+    if(objectOne.age > objectTwo){
+        console.log(`${objectOne.name} age is ${objectOne.age} which is more than ${objectOne.name}`)
+    }
+}
